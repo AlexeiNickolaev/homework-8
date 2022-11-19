39cbf582ce9493a36a3bec415efbfdb87d58f44c
Alexei <datlife2015@yandex.ru>
2022-11-19 16:18:38 +0300

    Треугольник Паскаля

--- 0008/Program.cs
+++ 0008/Program.cs
@@ -118,7 +118,36 @@ void Print3DMatrix(int[,,]matrix)
         Console.WriteLine("]");
     }
 }
-
+//Показать треугольник Паскаля. Сделать вывод в виде равнобедренного треугольника
+void FillTriangle(int[,] triangle) 
+{ 
+    for (int i = 1; i < triangle.GetLength(0); i++) 
+    {
+        for (int j = 1; j < i + 1; j++)
+        { 
+             triangle[i, j] = triangle[i - 1, j] + triangle[i - 1, j - 1]; 
+        } 
+        
+        for (int k = 1; k < triangle.GetLength(0); k++)  
+        { 
+            triangle[k, 0] = 1;
+        } 
+    }
+} 
+void PrintTriagle(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = array.GetLength(0); k > i; k--)
+            Console.Write("  ");
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] != 0)
+            {
+                Console.Write("{0,4}", array[i, j]);
+            }
+        Console.WriteLine();
+    }
+}
 
 Console.WriteLine("Найти произведение двух матриц");
 int[,] matr1 = CreateMatrix(4, 4, 1, 10);
@@ -154,3 +183,13 @@ Console.Clear();
 Console.WriteLine("Сформировать трехмерный массив и вывести его на экран");
 int[,,]matrix3D=Create3DMatrix(2, 2, 3, 1, 10);
 Print3DMatrix(matrix3D);
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("Показать треугольник Паскаля.Сделать вывод в виде равнобедренного треугольника");
+Console.Write("Введите кол-во строк: ");
+int n = int.Parse(Console.ReadLine() ?? "0"); 
+Console.WriteLine();
+int[,] triangle = new int[n+1, 2*n+1]; 
+FillTriangle(triangle); 
+PrintTriagle(triangle);
